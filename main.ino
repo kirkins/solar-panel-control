9a3e6ae5d261b7c2953e9e7804b10eeea6c3c9d7
Philip Kirkbride <kirkins@gmail.com>
2019-12-06 21:46:52 -0500

    change PWM for pin 10 to 30hz

--- main.ino
+++ main.ino
@@ -58,7 +58,41 @@ DallasTemperature sensors(&oneWire);
 // 1 = case
 // 2 = battery
 
+// setPwmFrequencyUNO function from:
+// https://github.com/fizcris/PWM_frequency_Arduino_change
+void setPwmFrequencyUNO(int pin, int divisor) {
+  byte mode;
+  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 2: mode = 0x02; break;
+      case 3: mode = 0x03; break;
+      case 4: mode = 0x04; break;
+      case 5: mode = 0x05; break;
+      default: return;
+    }
+    if(pin == 5 || pin == 6) {
+      TCCR0B = TCCR0B & 0b11111000 | mode;
+    } else {
+      TCCR1B = TCCR1B & 0b11111000 | mode;
+    }
+  } else if(pin == 3 || pin == 11) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 2: mode = 0x02; break;
+      case 3: mode = 0x03; break;
+      case 4: mode = 0x04; break;
+      case 5: mode = 0x05; break;
+      case 6: mode = 0x06; break;
+      case 7: mode = 0x07; break;
+      default: return;
+    }
+    TCCR2B = TCCR2B & 0b11111000 | mode;
+
+}}
+
 void setup() {
+  setPwmFrequencyUNO(10, 7)
   Serial.begin(9600);
   sensors.begin();
   pinMode(fanControl, OUTPUT);
